09be6163efb13b378bc6a04606ac795804530a29
HermannKagambega <hermannkagambega8@gmail.com>
2026-09-22 01:37:56 +0000

    test: prépare le conflit sur login.js

--- login.js
+++ login.js
@@ -1,8 +1,3 @@
 function login() {
-<<<<<<< HEAD
-console.log("version U3");
-=======
-  const username = document.getElementById("user").value;
-  console.log("Connexion de " + username);
->>>>>>> eb8d8f03d7974b01f696161d1184c23f55976716
+  console.log("version U3");
 }
